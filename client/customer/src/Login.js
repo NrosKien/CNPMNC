48b15604935232ae623862bea6a6c5d3188393aa
nrostrungkien3779@gmail.com <nrostrungkien3779@gmail.com>
2021-04-13 00:00:39 +0700

    customer v1

---
+++ client/customer/src/Login.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Login.css'
+
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
+
+export default function Login() {
+    const {register, formState:{errors}, handleSubmit} = useForm();
+
+    // get Data from Login and travel it to Server(NodeJS)
+    const onSubmit = async (data)=>{
+        try{
+            const response = await axios.post('http://localhost:8080/customer',data);
+            console.log(`response`,response);
+            this.props.history.push('/Main');
+        }
+        catch (error){
+            console.log(`error.response.data.message`,error.response.data.message);
+        }
+    };
+    return (
+        <div>
+            <form className='box' action="/" method='post' onSubmit ={handleSubmit(onSubmit)}>
+                <h2>Traveloka</h2>
+                <div className="box-login">
+                    <label>Email</label>
+                        <input type='email' placeholder="Enter your email here" {...register("email", {required: true})}></input>
+                            <span style={{color: 'red'}}>{errors.email && 'Email is required'}</span>
+                    <label>Password</label>
+                        <input type='password' placeholder="Enter your password here" {...register("password", {required: true})}></input>
+                            <span style={{color: 'red'}}>{errors.password && 'Password is required'}</span>
+                    <p><a href="/">Forget your password</a></p>
+                </div>
+                <input type='submit' name='' value='Login'></input>
+                <p>Không có tài khoản? <a href="/Register">Đăng ký ngay</a></p>              
+            </form>
+        </div>
+    )
+}
